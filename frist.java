0df8a7b4a1fa8e5520e02bffc4caa1ebf3fccad6
vivekVSH <handevivek942@gmail.com>
2025-05-16 14:41:35 +0530

    make changes in code

--- frist.java
+++ frist.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class frist {
-    
+    public static void main(String args[]){
+        System.out.println("Hello Vivek!!");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        int sub = a-b;
+        float div = a/b;
+        int mul = a*b;
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(div);
+        System.out.println(mul);
+    }
 }
