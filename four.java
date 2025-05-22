c19d1e42525546b7e7af459f9573d0401ad906c8
vivekVSH <handevivek942@gmail.com>
2025-05-22 11:31:31 +0530

    changes in file

--- four.java
+++ four.java
@@ -1,54 +1,59 @@
 public class four {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        /*frist
+        /*
+          frist
          * 
-         * * 
-         * * * 
-         * * * * 
-         * * * * *  
-        end patten */
+         * *
+         * * *
+         * * * *
+         * * * * *
+          end patten
+         */
 
         int row = 5;
-        for(int i=1;i<=row; i++){
-            for(int j=1; j<=i; j++){
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
         System.out.println();
 
-        /*second patten 
-        * * * * * 
-        * * * * * 
-        * * * * * 
-        * * * * * 
-        end patten*/
-
+        /*
+          second patten
+         * * * * *
+         * * * * *
+         * * * * *
+         * * * * *
+          end patten
+         */
 
         int num = 4;
-        for(int i=1; i<=num; i++){
-            for(int j=1; j<=5; j++){
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= 5; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
         System.out.println();
 
-        /*Third patten 
-        *****
-        *   *
-        *   *
-        *****
-        patten end*/
+        /*
+          Third patten
+         *****
+         *   *
+         *   *
+         *****
+          patten end
+         */
 
         int num1 = 4;
         int num2 = 5;
-        for(int i=1; i<=num1; i++){
-            for(int j=1; j<=num2; j++){
-                if(i == 1 || j == 1 || i == num1 || j == num2){
+        for (int i = 1; i <= num1; i++) {
+            for (int j = 1; j <= num2; j++) {
+                if (i == 1 || j == 1 || i == num1 || j == num2) {
                     System.out.print("*");
-                }else{
+                } else {
                     System.out.print(" ");
                 }
             }
@@ -56,41 +61,45 @@ public class four {
         }
         System.out.println();
 
-        /*four patten
-        * * * * *
-        * * * *
-        * * *
-        * *
-        *
-        end patten*/
+        /*
+          four patten
+         * * * * *
+         * * * *
+         * * *
+         * *
+         *
+          end patten
+         */
 
         int num3 = 5;
-        for(int i=num3; i>=1; i--){
-            for(int j=1; j<=i; j++){
+        for (int i = num3; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
         System.out.println();
 
-        /* five patten
-                *
-              * *
-            * * *
-          * * * *
-        * * * * *
-        end patten*/
+        /*
+          five patten
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+          end patten
+         */
 
-        int k = 5;
-        for(int i=1; i<=k; i++){
-            for(int j=1; j<=k-i; j++){
-                System.out.print(" ");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("  ");
             }
-            for(int j=1; j<=i; j++){
-                System.out.print("*");
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
+
         System.out.println();
 
     }
