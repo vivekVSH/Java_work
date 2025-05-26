ce58d29fcd1d977beedac3ee74ff0826737ad6b3
vivekVSH <handevivek942@gmail.com>
2025-05-26 15:37:14 +0530

    new change

--- six.java
+++ six.java
@@ -1,5 +1,26 @@
+import java.util.*;
+
 public class six {
+
+    public static long factorial(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+
+        return (n * factorial(n -1));
+    }
     public static void main(String[] args) {
-        
+
+        //sort array using userdefine method
+        int[] array = {2,4,5,3,24,2,3,5,5,8,96,5,4};
+        Arrays.sort(array);
+        System.out.print(Arrays.toString(array));
+        System.out.println();
+
+        //factorial using recursion
+        int n = 10;
+        System.out.println(factorial(n));
+
+        //
     }
 }
