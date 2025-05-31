ddec3b96c1704f8e365b74fa00b610ddf409b458
vivekVSH <handevivek942@gmail.com>
2025-05-31 12:50:26 +0530

    new changes

--- ten.java
+++ ten.java
@@ -1,4 +1,37 @@
+class animal{
+    String color;
+}
+
+class cat extends animal{
+    public void sound(){
+        System.out.print("meo meo");
+    }
+}
+
+//write multiple inhritance
+interface one{
+    void euu();
+}
+
+class two implements one{
+    public void euu(){
+
+    }
+}
+
+class three implements one{
+    public void euu(){
+
+    }
+} 
+
+class four extends two {
+    public void euu(){
+        super.euu();
+    }
+}
 public class ten {
+    // write basic code for inheritance
     public static void main(String[] args) {
         
     }
