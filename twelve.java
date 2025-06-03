d3b18830d68c09d9f50da348d2a851951fdc3a46
vivekVSH <handevivek942@gmail.com>
2025-06-03 22:45:59 +0530

    new changes

--- twelve.java
+++ twelve.java
@@ -1,5 +1,12 @@
 public class twelve {
     public static void main(String[] args){
-        
+        int[] number = {3,4,5,2,6,8,9,4};
+        int large = number[0];
+        for(int i=1; i<number.length; i++){
+            if(number[i] > large){
+                large = number[i];
+            }
+        }
+        System.out.println("This is Largest number in array : " + large);
     }
 }
