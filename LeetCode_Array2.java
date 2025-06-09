11e18fdb4afec8d2047b6a9bd21d49d1b242dd42
vivekVSH <handevivek942@gmail.com>
2025-06-09 15:22:08 +0530

    new program

--- LeetCode_Array2.java
+++ LeetCode_Array2.java
@@ -29,6 +29,26 @@ public class LeetCode_Array2 {
         }
         return nums[left];
     }
+
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] output = new int[n];
+
+        // Step 1: Calculate prefix products (left side)
+        output[0] = 1;
+        for (int i = 1; i < n; i++) {
+            output[i] = output[i - 1] * nums[i - 1];
+        }
+
+        // Step 2: Calculate suffix products (right side) and multiply with prefix
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            output[i] *= right;
+            right *= nums[i];
+        }
+
+        return output;
+    }
     public static void main(String[] args){
 
         String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
@@ -38,7 +58,18 @@ public class LeetCode_Array2 {
             System.out.println(group);
         }
 
+        //find minimum number in array
         int nums[] = {3,6,7,2,5,8,9};
         System.out.println(findMin(nums));
+
+        //find product all in element in array except i
+        LeetCode_Array2 sol = new LeetCode_Array2();
+        int[] nums1 = {1, 2, 3, 4};
+        int[] result1 = sol.productExceptSelf(nums1);
+
+        for (int val : result1) {
+            System.out.print(val + " ");
+        }
+
     }
 }
