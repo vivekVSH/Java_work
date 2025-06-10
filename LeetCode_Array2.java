2e243310bdb800c720f9cc445e9b790256c98088
vivekVSH <handevivek942@gmail.com>
2025-06-10 19:11:58 +0530

    temperature code added

--- LeetCode_Array2.java
+++ LeetCode_Array2.java
@@ -49,6 +49,21 @@ public class LeetCode_Array2 {
 
         return output;
     }
+
+    public static int[] dailyTemperature(int[] temperatures){
+        int n = temperatures.length;
+        int[] result = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int index = stack.pop();
+                result[index] = i - index;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
     public static void main(String[] args){
 
         String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
@@ -70,6 +85,13 @@ public class LeetCode_Array2 {
         for (int val : result1) {
             System.out.print(val + " ");
         }
+        System.out.println();
+
+        //calculate the temprature increase by day
+        int[] temperature = {73,74,75,71,69,72,76,73};
+        int[] res = dailyTemperature(temperature);
+        System.out.print("Result : " + Arrays.toString(res));
+        System.out.println();
 
     }
 }
