622f14c98fb1743b99f15e23d4589b68355c5211
vivekVSH <handevivek942@gmail.com>
2025-07-03 16:42:28 +0530

    both methos using implments using runnable interface and extends thread class

--- thread.java
+++ thread.java
@@ -1,6 +1,30 @@
+
+class word extends Thread{
+    public void run(){
+        for(int i=0; i<10; i++){
+            System.out.println("Word");
+        }
+    }
+}
+
+class pass implements Runnable{
+    public void run(){
+        System.out.print("Namste");
+    }
+}
 public class thread {
+
     public static void main(String[] args){
         System.out.println("Vivek");
+        for(int i=0; i<10; i++){
+            System.out.println("Vivek");
+        }
         System.out.println(Thread.currentThread().getName());
+        word wk = new word();
+        wk.start();
+
+        pass pk = new pass();
+        Thread T1 = new Thread(pk);
+        T1.start();
     }
 }
