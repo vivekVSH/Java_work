199d4609e96b87753f4e7d114cd1e6f9051e86f5
vivekVSH <handevivek942@gmail.com>
2025-07-06 20:14:05 +0530

    Queue code is added

---
+++ QueueY.java
@@ -0,0 +1,67 @@
+public class QueueY{
+
+    //queue is implement using Array
+    
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear = -1;
+
+        public Queue(int n){
+            arr = new int[n];
+            this.size = n;
+        }
+
+        public static boolean isEmpty(){
+            return rear == -1;
+        }
+
+        public static void add(int data){
+            if(rear == size -1){
+                System.out.println("Queue is full");
+                return;
+            }
+
+            rear++;
+            arr[rear] = data;
+
+        }
+
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("Queue is Empty");
+                return -1;
+            }
+
+            int front = arr[0];
+
+            for(int i=0; i<rear; i++){
+                arr[i] = arr[i+1];
+            }
+
+            rear--;
+            return rear;
+        }
+
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+
+            return arr[0];
+        }
+    }
+    public static void main(String args[]){
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+
+    }
+}
