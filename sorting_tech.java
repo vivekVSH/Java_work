7fb1f6d5f3c399d0dfe018567008ec99f4a88744
vivekVSH <handevivek942@gmail.com>
2025-07-08 02:21:03 +0530

    insertion sort code add

--- sorting_tech.java
+++ sorting_tech.java
@@ -39,10 +39,42 @@ public class sorting_tech {
             arr[prev+1] = curr;
         }
     }
+
+    public static int partition(int[] arr, int start, int end){
+        int piot = arr[end];
+        int idx = start - 1;
+
+        for(int i=start; i<end-1; i++){
+            if(arr[i] < piot){
+                idx++;
+                swap(arr, idx, i);
+            }
+        }
+        swap(arr, idx, end);
+        return idx++;
+    }
+
+    public static void swap(int[] arr, int i , int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void quick(int[] arr, int start, int end){
+        if(start < end){
+            int piot = partition(arr, start, end);
+            quick(arr, start, piot-1);
+            quick(arr, piot+1, end);
+        }
+    }
+
+
     public static void main(String[] args){
         int[] arr = {1,3,2,6,4,9,5};
+        int n = arr.length;
         selection(arr);
         bubble(arr);
         insertion(arr);
+        quick(arr,0,n);
     }
 }
