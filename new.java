5d8987662f426c452941165ae03147af7cf3bf30
vivekVSH <handevivek942@gmail.com>
2025-08-09 00:44:57 +0530

    random function code

---
+++ new.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            Random rm = new Random();
+            int p = rm.nextInt(nums.length);
+            int q = rm.nextInt(nums.length);
+            if (p > 0 && p < q && q < n - 1) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+    }
+}
